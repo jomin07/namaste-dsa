9392f5297a9add9e044822aeb7c8c8ff0c0f79b7
jomin07 <107167492+jomin07@users.noreply.github.com>
2026-01-09 23:33:07 +0530

    day 06 - removeElement & reverseString done

---
+++ arrays/reverseString.js
@@ -0,0 +1,18 @@
+// 344. Reverse String
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+
+var reverseString = function (s) {
+  let len = s.length;
+  for (let i = 0; i < len / 2; i++) {
+    let temp = s[i];
+    s[i] = s[len - 1 - i];
+    s[len - 1 - i] = temp;
+  }
+};
